a834c6e91d200c5602d3433ecc91fd9e3f5b0293
admin <laurentiu@eigodev.ro>
2016-05-11 23:27:33 +0300

    Add database sqlite with ef

--- CryptoApp/Program.cs
+++ CryptoApp/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.Entity;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CryptoApp
 {
@@ -10,6 +9,52 @@ namespace CryptoApp
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
         }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            optionsBuilder.UseSqlite("Filename=" + Path.Combine(path, "blog.db"));
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
     }
 }
